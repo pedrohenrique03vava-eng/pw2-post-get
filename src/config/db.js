fe2881208c25872598aebc2aa4791e60455b368c
Pedro Verne <pedrohenrique03vava@gmail.com>
2026-08-01 21:15:00 -0300

    adicionando .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
+import "dotenv/config";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "9632587410Ph@",
-  database: "tarefas",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 const createTableQuery = `
